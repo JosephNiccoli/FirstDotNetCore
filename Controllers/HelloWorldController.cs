0fc0bdab61a1c5b03809c41f809153ddf7ef5e62
jniccoli.thermalstructures <jniccoli.thermalstructures@gmail.com>
2019-08-29 13:59:12 -0700

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}"); // Uses HtmlEncoder.Default.Encode to protect the app from malicious input (namely JavaScript).
+        //}
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ Data/HelloWorldContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HelloWorld.Models;  // Enables public DbSet<Movie> Movie
+
+namespace HelloWorld.Models
+{
+    public class HelloWorldContext : DbContext
+    {
+        public HelloWorldContext (DbContextOptions<HelloWorldContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
